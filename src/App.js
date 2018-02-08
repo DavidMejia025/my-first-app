df645ce4063ad0fe95e0c2c2d84a385c77e48d9e
David Mejia <davidndy@gmail.com>
2018-02-07 20:20:28 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Esta es mi primera app con React</h1>
+        <p> Esto es un hola mundo en React </p>
+      </div>
+    );
+  }
+}
+
+export default App;
