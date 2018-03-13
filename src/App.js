2f21fb0d487bb75293894e6b42d958ddec38a6f0
DavidMejia025 <davidndy@gmail.com>
2018-03-12 21:29:59 -0500

    First commit on this app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+// I dont have any idea on how react works
 class App extends Component {
   render() {
     return (
       <div>
-        <h1> Mi primera aplicación</h1>
+        <h1> Mi primera aplicación</h1> 
         <p> Esta es mi primera aplicación en React, está padrísimo! </p>
       </div>
     );
   }
 }
 
-export default App; 
+export default App;  
+ 
