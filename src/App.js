0d2584083c489759acc67811ec557d08c70044ea
david <davidndy@gmail.com>
2019-11-26 18:55:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0
+    };
+
+    this.getText = this.getText.bind(this);
+  }
+
+  getText(e){
+    var string = e.target.value;
+
+    this.setState({ count: string.length });
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.getText}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
   }
